bd733229b3d5d0c1ab6ba2bcb9489b085e13d500
Samantha Mathis <Samantha122@email.arizona.edu>
2021-05-04 10:13:36 -0700

    start project2

---
+++ db.js
@@ -0,0 +1,86 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+
+let host = process.env.host;
+let database = process.env.database;
+let port = process.env.port;
+let username = process.env.username;
+let password = process.env.password;
+
+let connectionString = 
+`postgres://${username}:${password}@${host}:${port}/${database}`;
+
+let connection = {
+    connectionString: process.env.DATABASE_URL ? process.env.DATABASE_URL : connectionString,
+    ssl : {rejectUnauthorized: false}
+};
+
+const pool = new Pool(connection);
+
+let saveAddress = (street, city, state, postalCode) => {
+    return pool.query('insert into mynearbyplaces.address(street, city, state, postalcode) values ($1, $2, $3, $4) returning id', [street, city, state, postalCode])
+    .then(result => {console.log('the address was saved'); return result.rows[0].id});
+   
+}
+
+let savePlace = (name, addressId) => 
+{
+    return pool.query('insert into mynearbyplaces.place(name, addressid) values ($1, $2)', [name, addressId])
+    .then(() => {console.log('the place was saved'); });    
+
+}
+
+let saveReview = (comment, rating, placeId) => {
+    return pool.query('insert into mynearbyplaces.review(comment, rating, placeid) values ($1, $2, $3)', [comment, rating, placeId])
+    .then(result => {console.log('the review was saved'); return result.rows[0].id});
+   
+}
+let getCustomerId = (name) =>{
+    let sql = `select id from mynearbyplaces.customer c where c.name = '${name}'`
+    return pool.query(sql)
+    .then(result => result.rows);
+}
+
+let getPlaceId = (placeName) =>{
+    let sql = `select id from mynearbyplaces.place p where p.name = '${placeName}'`
+    return pool.query(sql)
+    .then(result => result.rows);
+}
+let getPlaces = () => {
+    let sql = `select p.name, a.street , a.city , a.state , a.postalcode, 
+    json_agg(json_build_object('comment', r.comment, 'rating', r.rating, 'customer', c."name")) as reviews
+    from mynearbyplaces.place p 
+    inner join mynearbyplaces.address a on p.addressid  = a.id 
+    inner join mynearbyplaces.review r on p.id = r.placeid 
+    left join mynearbyplaces.customer c on r.customerid = c.id 
+    group by p.name, a.street , a.city , a.state , a.postalcode`;
+    return pool.query(sql)
+    .then(result => result.rows);
+    
+}
+
+let findPlaces = (name, street, city, state, postalcode) => {
+
+    let sql = `select p.name, a.street , a.city , a.state , a.postalcode, 
+    json_agg(json_build_object('comment', r.comment, 'rating', r.rating, 'customer', c."name")) as reviews
+    from mynearbyplaces.place p 
+    inner join mynearbyplaces.address a on p.addressid  = a.id 
+    left join mynearbyplaces.review r on p.id = r.placeid 
+    left join mynearbyplaces.customer c on r.customerid = c.id 
+    where 
+    (lower(p.name) like lower('${!name ? '%%' : `%${name}%`}')) 
+    and (lower(a.street) like lower('${!street ? '%%' : `%${street}%`}')) 
+    and (lower(a.city) like lower('${!city ? '%%' : `%${city}%`}')) 
+    and (lower(a.state) like lower('${!state ? '%%' : `%${state}%`}')) 
+    and (cast(a.postalcode as text) like '${!postalcode ? '%%' : `%${postalcode}%`}') 
+    group by p.name, a.street , a.city , a.state , a.postalcode
+    `;
+    console.log(sql);
+    return pool.query(sql)
+    .then(result => result.rows);
+
+}
+
+
+module.exports = { saveAddress, savePlace, getPlaces, findPlaces, saveReview, getPlaceId, getCustomerId}
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 4002;
+
+app.get('/', (request, response) =>{
+    response.send(`Welcome to Imagequiz-Serverside`);
+})
+
+
+app.post('/place', (request, response) => {  
+    let name = request.body.name;
+    let street = request.body.street; 
+    let city = request.body.city;
+    let state = request.body.state;
+    let postalcode = request.body.postalcode; 
+    db.saveAddress(street, city, state, postalcode)
+    .then(addressid => db.savePlace(name, addressid))
+    .then(() => response.send('The place was added.'))
+    .catch(e => {console.log(e); response.status(500).send('There was an error in adding the place.')});
+
+});
+
+app.post('/review/:placeName', (request, response) => {  
+    let placeName = request.params.placeName;
+    let name = request.body.name;
+    let comment = request.body.comment
+    let rating = request.body.rating
+    db.getPlaceId(placeName)
+    .then(customerid => getCustomerId(name))
+    .then(placeid => saveReview(comment,rating,placeid, customerid))                  
+    .then(() => response.send('The review was added.'))
+    .catch(e => {console.log(e); response.status(500).send('There was an error in adding the place.')});
+
+});
+
+app.get('/places', (request, response) => {  
+    db.getPlaces()
+    .then(places => response.json(places))
+    .catch(e => {console.log(e); response.status(500).send('There was an error in finding the places.')});  
+   
+  });
+
+  app.get('/search/:name?/:street?/:city?/:state?/:postalcode?', (request, response) => {  
+    let name = request.query.name;
+    let street = request.query.street;
+    let city = request.query.city;
+    let state = request.query.state;
+    let postalcode = request.query.postalcode;
+    console.log(`name: ${name} - ${typeof(name)}`);
+    console.log(`street: ${street} - ${typeof(street)}`);
+    db.findPlaces(name, street, city, state, postalcode)
+    .then(places => response.json(places))
+    .catch(e => {console.log(e); response.status(500).send('There was an error in finding the places.')});  
+    
+  });
+
+
+app.listen(port, () => console.log("Listening on port " + port))
