2496847e4e51705f20dccfb1678bb713c9de02e9
Samantha Mathis <Samantha122@email.arizona.edu>
2021-05-05 01:36:56 -0700

    update 5/5

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg');
 let host = process.env.host;
 let database = process.env.database;
 let port = process.env.port;
-let username = 'hekgokddlntpp';
+let username = 'chekgokddlntpp';
 let password = process.env.password;
 
 let connectionString = 
@@ -19,42 +19,48 @@ let connection = {
 const pool = new Pool(connection);
 
 let saveAddress = (street, city, state, postalCode) => {
-    return pool.query('insert into mynearbyplaces.address(street, city, state, postalcode) values ($1, $2, $3, $4) returning id', [street, city, state, postalCode])
+    return pool.query('insert into mynearbyplaces.address(street, city, state, zip) values ($1, $2, $3, $4) returning id', [street, city, state, postalCode])
     .then(result => {console.log('the address was saved'); return result.rows[0].id});
    
 }
 
 let savePlace = (name, addressId) => 
 {
-    return pool.query('insert into mynearbyplaces.place(name, addressid) values ($1, $2)', [name, addressId])
+    return pool.query('insert into mynearbyplaces.place(placename, addressid) values ($1, $2)', [name, addressId])
     .then(() => {console.log('the place was saved'); });    
 
 }
 
-let saveReview = (comment, rating, placeId) => {
-    return pool.query('insert into mynearbyplaces.review(comment, rating, placeid) values ($1, $2, $3)', [comment, rating, placeId])
-    .then(result => {console.log('the review was saved'); return result.rows[0].id});
+let saveReview = (comment, rating, placeId, customerId) => {
+    return pool.query('insert into mynearbyplaces.review(comment, rating, placeid, customerid) values ($1, $2, $3, $4)', [comment, rating, placeId, customerId])
+    .then(() => {console.log('the review was saved');});
    
 }
-let getCustomerId = (name) =>{
+let getCustomerId = (name) => {
     let sql = `select id from mynearbyplaces.customer c where c.name = '${name}'`
     return pool.query(sql)
-    .then(result => result.rows);
+    .then(result => result.rows[0].id);
+}
+
+let getAddressId = (street, city, state, zip) => {
+    let sql = `select id from mynearbyplaces.address a where a.street= '${street}' and a.city='${city}' and a.state='${state}' and a.zip=${zip}`
+    return pool.query(sql)
+    .then(result => result.rows[0].id);
 }
 
 let getPlaceId = (placeName) =>{
-    let sql = `select id from mynearbyplaces.place p where p.name = '${placeName}'`
+    let sql = `select id from mynearbyplaces.place p where p.placename = '${placeName}'`
     return pool.query(sql)
-    .then(result => result.rows);
+    .then(result => result.rows[0].id);
 }
 let getPlaces = () => {
-    let sql = `select p.name, a.street , a.city , a.state , a.postalcode, 
+    let sql = `select p.placename, a.street , a.city , a.state , a.zip, 
     json_agg(json_build_object('comment', r.comment, 'rating', r.rating, 'customer', c."name")) as reviews
     from mynearbyplaces.place p 
     inner join mynearbyplaces.address a on p.addressid  = a.id 
     inner join mynearbyplaces.review r on p.id = r.placeid 
     left join mynearbyplaces.customer c on r.customerid = c.id 
-    group by p.name, a.street , a.city , a.state , a.postalcode`;
+    group by p.name, a.street , a.city , a.state , a.zip`;
     return pool.query(sql)
     .then(result => result.rows);
     
@@ -62,7 +68,7 @@ let getPlaces = () => {
 
 let findPlaces = (name, street, city, state, postalcode) => {
 
-    let sql = `select p.name, a.street , a.city , a.state , a.postalcode, 
+    let sql = `select p.placename, a.street , a.city , a.state , a.zip, 
     json_agg(json_build_object('comment', r.comment, 'rating', r.rating, 'customer', c."name")) as reviews
     from mynearbyplaces.place p 
     inner join mynearbyplaces.address a on p.addressid  = a.id 
@@ -74,18 +80,57 @@ let findPlaces = (name, street, city, state, postalcode) => {
     and (lower(a.city) like lower('${!city ? '%%' : `%${city}%`}')) 
     and (lower(a.state) like lower('${!state ? '%%' : `%${state}%`}')) 
     and (cast(a.postalcode as text) like '${!postalcode ? '%%' : `%${postalcode}%`}') 
-    group by p.name, a.street , a.city , a.state , a.postalcode
+    group by p.name, a.street , a.city , a.state , a.zip
     `;
     console.log(sql);
     return pool.query(sql)
     .then(result => result.rows);
 
 }
-let deletePlace = (id) => {
-    console.log(connectionString)
-    let sql = `DELETE FROM mynearbyplaces.place WHERE id=$1`;
+let deletePlace = (addressId) => {
+    let sql = `DELETE FROM mynearbyplaces.place p WHERE p.addressid=${addressId}`;
     return pool.query(sql)
-    .then(result => {console.log('the place was deleted'); });  
+    .then(result  => addressId);  
 }
 
-module.exports = { saveAddress, savePlace, getPlaces, findPlaces, saveReview, getPlaceId, getCustomerId, deletePlace}
+let deleteAddress = (addressId) => {
+
+    let sql = `DELETE FROM mynearbyplaces.address WHERE id=${addressId}`;
+    return pool.query(sql)
+    .then(result => {console.log('the address was deleted'); });  
+}
+
+let deleteReview = (text, rating, customerid) => {
+    let sql = `DELETE FROM mynearbyplaces.review WHERE comment='${text}' and rating='${rating}' and customerid=${customerid}`;
+    return pool.query(sql)
+    .then(result => {console.log('the review was deleted'); });  
+}
+
+let addCustomer = (name, email, password) => {
+    return pool.query('insert into mynearbyplaces.customer(name, email, password) values ($1, $2, $3)', [name, email, password])
+    .then(() => console.log('The customer was saved.'))
+    .catch(e => console.log(e));
+}
+
+let updatePlace =(street, city, state, zip, addressid) =>{
+    let sql = `UPDATE mynearbyplaces.address set street='${street}',
+    city='${city}',state='${state}', zip=${zip} where id=${addressid}`
+    return pool.query(sql)
+    .then(result => {console.log('the address is updated')});
+}
+
+
+module.exports = { 
+    saveAddress, 
+    savePlace, 
+    getPlaces, 
+    findPlaces, 
+    saveReview, 
+    getPlaceId, 
+    getCustomerId, 
+    deletePlace, 
+    addCustomer, 
+    deleteReview,
+    getAddressId, 
+    deleteAddress,
+    updatePlace}
--- server.js
+++ server.js
@@ -7,14 +7,15 @@ const db = require('./db');
 
 app.use(express.json());
 app.use(cors());
-
+var crypto = require('crypto');
 
 app.get('/', (request, response) =>{
-    response.send(`Welcome to Imagequiz-Serverside`);
+    response.send(`Welcome to nearbyplaces-Serverside`);
 })
 
 
 app.post('/place', (request, response) => {  
+    console.log("hello");
     let name = request.body.name;
     let street = request.body.street; 
     let city = request.body.city;
@@ -27,16 +28,19 @@ app.post('/place', (request, response) => {
 
 });
 
-app.post('/review/:placeName', (request, response) => {  
+app.post('/review/:placeName', async (request, response) => {  
     let placeName = request.params.placeName;
     let name = request.body.name;
-    let comment = request.body.comment
-    let rating = request.body.rating
-    db.getPlaceId(placeName)
-    .then(customerid => getCustomerId(name))
-    .then(placeid => saveReview(comment,rating,placeid, customerid))                  
+    let comment = request.body.comment;
+    let rating = request.body.rating;
+    let placeId;
+    await db.getPlaceId(placeName)
+    .then(placeid => placeId = placeid)
+
+    db.getCustomerId(name)
+    .then(customerid => db.saveReview(comment, rating, placeId, customerid))                  
     .then(() => response.send('The review was added.'))
-    .catch(e => {console.log(e); response.status(500).send('There was an error in adding the place.')});
+    .catch(e => {console.log(e); response.status(500).send('There was an error in adding the review.')});
 
 });
 
@@ -53,17 +57,46 @@ app.get('/places', (request, response) => {
     let city = request.query.city;
     let state = request.query.state;
     let postalcode = request.query.postalcode;
-    console.log(`name: ${name} - ${typeof(name)}`);
-    console.log(`street: ${street} - ${typeof(street)}`);
     db.findPlaces(name, street, city, state, postalcode)
     .then(places => response.json(places))
     .catch(e => {console.log(e); response.status(500).send('There was an error in finding the places.')});  
     
   });
-  app.post('/deletePlace', (request, response) => {
-    db.deletePlace(request.body.id)
-    .then(places => response.json(places))
+  app.post('/deletePlace', async(request, response) => {
+      //let addressId;
+    db.getAddressId(request.body.street, request.body.city, request.body.state, request.body.zip)
+    .then(addressid  => db.deletePlace(addressid))
+    .then(addressID => db.deleteAddress(addressID))
     .catch(e => {console.log(e); response.status(500).send('there was an error in delete the place')})
  });
+
+ app.post('/deleteReview', (request, response) => {
+    db.getCustomerId(request.body.username)
+    .then(customerid => db.deleteReview(request.body.reviewName, request.body.rating, customerid))
+    .then(() => response.send('The review was deleted.'))
+    .catch(e => {console.log(e); response.status(500).send('there was an error in delete the place')})
+ });
+
+ app.post('/customer', (request, response) => {
+    let name = request.body.name;
+    let email = request.body.email;
+    var password = crypto.createHash('md5').update(request.body.password).digest('hex');
+    db.addCustomer(name, email, password)
+    .then(() => response.send('The customer was added.'))
+    .catch(e => response.status(500).send('There was an error in adding the customer'));
+});
+
+app.post('/updatePlace', (request, response) => {
+    let placeName = request.body.newAdd.placeName;
+    let street = request.body.newAdd.street; 
+    let city = request.body.newAdd.city;
+    let state = request.body.newAdd.state;
+    let zip= request.body.newAdd.zip; 
+    console.log("inside", request.body.oldAdd.zip);
+    db.getAddressId(request.body.oldAdd.street, request.body.oldAdd.city, request.body.oldAdd.state, request.body.oldAdd.zip)
+    .then(addressid  => db.updatePlace(street, city, state, zip, addressid))
+    .then(() => response.send('The place was updated.'))
+    .catch(e => response.status(500).send('There was an error in updating place'));
+});
 
 app.listen(port, () => console.log("Listening on port " + port))
